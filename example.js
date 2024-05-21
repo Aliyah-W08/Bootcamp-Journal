05ff2640b471b36f01d8393f28d9db0ebb8aa0d1
Aliyah-W08 <aliyahwaris278@gmail.com>
2024-05-21 15:06:47 +0100

    second commit

---
+++ example.js
@@ -0,0 +1,23 @@
+
+function toggle() {
+  let imgs = document.getElementById("imgbutton");
+
+  // Toggle the display property of the image
+  if (imgs.style.display === "none") {
+      imgs.style.display = "block";
+  } else {
+      imgs.style.display = "none";
+  }
+}
+
+
+function toggle2() {
+  let imgs = document.getElementById("imgbutton2");
+
+  // Toggle the display property of the image
+  if (imgs.style.display === "none") {
+      imgs.style.display = "block";
+  } else {
+      imgs.style.display = "none";
+  }
+}
